112b42edcc155154d362c841f87c33a2d7fd4c4c
denizbabig <denizbabig@gmail.com>
2025-11-02 21:17:33 +0100

    added map features

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import 'maplibre-gl/dist/maplibre-gl.css'
 
 const app = createApp(App)
 app.use(createPinia())
