6d4d36eeaa870c256f65f74ab7af3aceb3c1e5d2
denizbabig <denizbabig@gmail.com>
2025-11-02 21:18:20 +0100

    minor changes for view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
+<script setup lang="ts">
+import AddMarker from '../components/AddMarker.vue'
+import MarkerList from '../components/MarkerList.vue'
+import MapView from '../components/MapView.vue'
+</script>
+
 <template>
   <main>
+    <h1>Evocation Map</h1>
+    <MapView />
     <AddMarker />
     <MarkerList />
   </main>
 </template>
-
-<script setup lang = "ts">
-import AddMarker from '../components/AddMarker.vue'
-import MarkerList from '../components/MarkerList.vue'
-</script>
