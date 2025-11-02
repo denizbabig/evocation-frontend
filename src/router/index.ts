b30ea52f11f7765ea618bfd56179944d0ab187b1
denizbabig <denizbabig@gmail.com>
2025-11-02 21:17:19 +0100

    cleaner code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,11 @@
+// src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MapLayout from '../views/MapLayout.vue'
 
-const router = createRouter({
+export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutEvocation.vue'),
-    },
+    { path: '/', name: 'map', component: MapLayout },
+    { path: '/about', name: 'about', component: () => import('../views/AboutEvocation.vue') }
   ],
 })
-
-export default router
