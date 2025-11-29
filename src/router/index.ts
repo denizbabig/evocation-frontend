d23337422eacea1eb1651d2af527e9409518e96c
denizbabig <denizbabig@gmail.com>
2025-11-29 23:30:57 +0100

    routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,8 @@ import MapLayout from '@/views/MapLayout.vue'
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'map', component: MapLayout },
+    { path: '/', name: 'home', component: Homepage },
+    { path: '/map', name: 'map', component: MapLayout },
     { path: '/about', name: 'about', component: () => import('../views/AboutEvocation.vue') }
   ],
 })
