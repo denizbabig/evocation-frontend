900d7e6138c3dd64a1c680673010518d037887a9
denizbabig <denizbabig@gmail.com>
2025-12-18 20:33:37 +0100

    deleted

--- src/views/MapLayout.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="relative h-screen w-screen bg-[#0f1117]">
-    <MapView class="absolute inset-0" />
-  </div>
-</template>
-
-<script setup lang="ts">
-import MapView from '@/components/MapView.vue'
-</script>
