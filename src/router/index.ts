11ec4cb7e1c187c0e61386a56475d93a60d7abeb
denizbabig <denizbabig@gmail.com>
2025-12-28 12:20:29 +0100

    deleted

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,7 +44,6 @@ const router = createRouter({
     { path: '/trips/:id', name: 'TripDetail', component: TripDetailView },
     { path: '/marker', name: 'Marker', component: MarkersView },
     { path: '/dashboard', name: 'Dashboard', component: DashboardView },
-    { path: '/lab/overlays', name: 'OverlayLab', component: () => import('@/views/OverlayLabView.vue') },
   ],
 })
 
